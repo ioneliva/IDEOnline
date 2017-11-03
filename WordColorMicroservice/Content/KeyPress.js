48028d40179b927dd08c7f0bd2ebc5c108752a71
ionel iva <jhonel83@yahoo.com>
2017-11-03 07:24:15 +0200

    adding basic client, Nancy get */ post */ routes. Some functionality provided

---
+++ WordColorMicroservice/Content/KeyPress.js
@@ -0,0 +1,39 @@
+﻿var word = "";
+function inputKeyPress(e) {
+    var c;
+
+    //get the code for last key pressed. (unicode a=97, b=98, etc)
+    if (window.event) { // IE                    
+        c = e.keyCode;
+    } else if (e.which) { // Netscape/Firefox/Opera                   
+        c = e.which;
+    }
+    //convert character code to corresponding string from unicode, will be removed later, read TODO below
+    c = String.fromCharCode(c);
+
+    //valid chars for a word are [0-9a-zA-Z]+directional arrows+backspace
+    /*make regex expression based on char unicode. Numbers+letters+special characters are [48-57],[65-90],[97-122]. Also need to add rules so you can't declare a variable like 0abc. As of now, backspace is considered invalid...
+    for now, we'll go with converting them'
+    */
+    var wordComponent = /^[0-9a-zA-Z]+$/; //backspace is \b, ^ marks start of word, + represents "any number of iterations", $marks the end of the matching string
+    if (!c.match(wordComponent) && c!="\b" && word!="") {
+    //post
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:58579/", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(word);
+    //get answer from post
+    xhr.onload = function () {
+        var output = document.getElementById("output");
+        output.innerText =  this.responseText;
+    }
+    //TODO** 
+    //replacing the word in text with the word we got from server  "reserved"->"<span style="color: green">reserved</span>". For now we just return the word as it is, for testing
+    word = "";
+    } else {
+    //build a word
+    word += c;
+    }
+
+
+}
---
+++ WordColorMicroservice/Modules/ColorModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+using System.Text;
+using Nancy.ModelBinding;
+using Nancy.Extensions;
+
+
+
+namespace WordColorMicroservice.Modules
+{
+    public class ColorModule:NancyModule
+    {
+        public ColorModule()
+        {
+            Get("/", _ => Response.AsFile("Content/clientPage.html", "text/html")
+            );
+
+            //simple post request handle. returning the post request body, just for testing. For now
+            Post("/", _ =>
+            {
+                var body = this.Request.Body; //this is the actual post content
+                int length = (int)body.Length; // this is a dynamic variable, needs cast every time
+                byte[] data = new byte[length];
+                body.Read(data, 0, length);
+                string postBoddy = System.Text.Encoding.Default.GetString(data);
+                return "<span style=\"color: green\">" + postBoddy + "</span>";
+            });
+        }
+    }
+}
+
