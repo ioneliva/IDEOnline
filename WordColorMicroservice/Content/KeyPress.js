b7c2fa8d5d1aa89d7119de095bc89679fe146d9f
ionel iva <jhonel83@yahoo.com>
2017-11-17 17:16:37 +0200

    made changes to client javascript. It now recognizes words properly. Also sends separators to server for further analysis. The js also contains a prototype for a replaces funtion. Will use it to replace a word with decorated word in the future

--- WordColorMicroservice/Content/KeyPress.js
+++ WordColorMicroservice/Content/KeyPress.js
@@ -8,32 +8,76 @@ function inputKeyPress(e) {
     } else if (e.which) { // Netscape/Firefox/Opera                   
         c = e.which;
     }
-    //convert character code to corresponding string from unicode, will be removed later, read TODO below
+
+    //check if mouse was pressed, will use it later
+    var mousePressed = 230;
+    document.body.onmousedown = function () {
+        mousePressed = 1;
+        console.log("pressed mouse");
+    }
+    document.body.onmouseup = function () {
+        mousePressed = 0;
+        console.log("released mouse");
+    }
+
+    //convert character code to corresponding string from unicode, will be removed later in favor of unicode comparison TODO
     c = String.fromCharCode(c);
 
-    //valid chars for a word are [0-9a-zA-Z]+directional arrows+backspace
-    /*make regex expression based on char unicode. Numbers+letters+special characters are [48-57],[65-90],[97-122]. Also need to add rules so you can't declare a variable like 0abc. As of now, backspace is considered invalid...
-    for now, we'll go with converting them'
-    */
-    var wordComponent = /^[0-9a-zA-Z]+$/; //backspace is \b, ^ marks start of word, + represents "any number of iterations", $marks the end of the matching string
-    if (!c.match(wordComponent) && c!="\b" && word!="") {
-    //post
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:58579/", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(word);
+    var xhr = new XMLHttpRequest();//used for posting
+
+    if (c.match(/[a-zA-Z]+/)) { //problem here with backspace, word registeres chars that are deleted TODO treat the case backspace is used
+        word += c; //build a word
+    }
+    else {
+        //we have a delimiter read in c, post the word
+        if (!!word.trim()) { //word must not be blank, otherwise the user just pressed space/tab/enter a few times          
+            xhr.open("POST", "http://localhost:58579/", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(word);
+            console.log("sent post (word): " + word);
+        } 
+        if (c.match(/[\[\].,\/#!$%\^&\*;:{}=\-_`~()<>]/)) { //if delimiter is not a blank,space,tab,etc. post it  
+            xhr.open("POST", "http://localhost:58579/", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(c);
+            console.log("sent post (delimiter): " + c);
+        }
+        word = ""; //get ready for the next word
+    }
+
     //get answer from post
     xhr.onload = function () {
         var output = document.getElementById("output");
-        output.innerText =  this.responseText;
-    }
-    //TODO** 
-    //replacing the word in text with the word we got from server  "reserved"->"<span style="color: green">reserved</span>". For now we just return the word as it is, for testing
-    word = "";
-    } else {
-    //build a word
-    word += c;
+        output.innerText = this.responseText;
+
     }
 
+    //replace first parameter with second
+    function replaceWord(search, replace) {
+        var sel = window.getSelection();
+        if (!sel.focusNode) {
+            return;
+        }
+
+        var startIndex = sel.focusNode.nodeValue.indexOf(search);
+        var endIndex = startIndex + search.length;
+        if (startIndex === -1) {
+            return;
+        }
+        console.log("first focus node: ", sel.focusNode.nodeValue);
+        var range = document.createRange();
+        //Set the range to contain search text
+        range.setStart(sel.focusNode, startIndex);
+        range.setEnd(sel.focusNode, endIndex);
+        //Delete search text
+        range.deleteContents();
+        console.log("focus node after delete: ", sel.focusNode.nodeValue);
+        //Insert replace text
+        range.insertNode(document.createTextNode(replace));
+        console.log("focus node after insert: ", sel.focusNode.nodeValue);
+        //Move the caret to end of replace text
+        sel.collapse(sel.focusNode, 0);
+    }
 
 }
+
